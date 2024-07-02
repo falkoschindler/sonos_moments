169c46b073350212fc0966f20471ab6fa5a19604
Falko Schindler <falko@zauberzeug.com>
2024-07-02 17:42:17 +0200

    handle missing speakers

--- sonos_moments/speakers.py
+++ sonos_moments/speakers.py
@@ -3,8 +3,13 @@ from nicegui import ui
 
 
 def speakers_ui() -> None:
+    zones = soco.discover()
+    if not zones:
+        ui.label('No Sonos speakers found.')
+        return
+
     with ui.grid(columns='repeat(4, auto)').classes('items-center gap-x-1 gap-y-0 border shadow px-4 w-full overflow-hidden'):
-        for zone in sorted(soco.discover(), key=lambda z: z.player_name):
+        for zone in sorted(zones, key=lambda z: z.player_name):
             ui.label(zone.player_name).classes('text-lg font-bold')
             ui.checkbox(value=not zone.mute, on_change=lambda e, zone=zone: setattr(zone, 'mute', not e.value)) \
                 .props('dense checked-icon="volume_up" unchecked-icon="volume_off"').classes('-mr-4')
