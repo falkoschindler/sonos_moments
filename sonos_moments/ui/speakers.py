1787f85847f6632920487d0321fc6689ab02ff84
Falko Schindler <falko@zauberzeug.com>
2024-11-10 16:40:10 +0100

    joining and unjoining works (kind of)

--- sonos_moments/ui/speakers.py
+++ sonos_moments/ui/speakers.py
@@ -1,8 +1,9 @@
-from nicegui import app, ui
+from nicegui import app, events, ui
 
 from ..model import System
 
 
+@ui.refreshable
 def speakers_ui(system: System) -> None:
     if not system.devices:
         ui.label('No Sonos speakers found.')
@@ -10,8 +11,7 @@ def speakers_ui(system: System) -> None:
 
     with ui.tabs() as tabs:
         for key, devices in system.device_groups.items():
-            coordinator = next((d for d in devices if d.is_coordinator), None)
-            assert coordinator is not None
+            coordinator = next((d for d in devices if d.is_coordinator), devices[0])
             label = coordinator.player_name
             if len(devices) > 1:
                 label += f' + {len(devices) - 1}'
@@ -20,15 +20,36 @@ def speakers_ui(system: System) -> None:
     with ui.tab_panels(tabs).bind_value(app.storage.user, 'group').classes('w-full'):
         for key, devices in system.device_groups.items():
             with ui.tab_panel(key).classes('gap-x-1 border shadow w-full p-0 gap-y-0'):
-                coordinator = next((d for d in devices if d.is_coordinator), None)
-                assert coordinator is not None
-                with ui.button_group().props('flat').classes('m-4'):
-                    ui.button(icon='play_arrow', on_click=coordinator.play).props('flat')
-                    ui.button(icon='pause', on_click=coordinator.pause).props('flat')
-                    ui.button(icon='stop', on_click=coordinator.stop).props('flat')
+                coordinator = next((d for d in devices if d.is_coordinator), devices[0])
+                with ui.row(align_items='center').classes('m-4'):
+                    with ui.button_group().props('flat'):
+                        ui.button(icon='play_arrow', on_click=coordinator.play).props('flat')
+                        ui.button(icon='pause', on_click=coordinator.pause).props('flat')
+                        ui.button(icon='stop', on_click=coordinator.stop).props('flat')
+                    with ui.button(icon='speaker_group').props('flat'):
+                        with ui.menu():
+                            for device in sorted(system.devices.values(),
+                                                 key=lambda d: (d is not coordinator, d.player_name)):
+                                with ui.menu_item():
+                                    def update_group(e: events.ValueChangeEventArguments,
+                                                     device=device,
+                                                     coordinator=coordinator) -> None:
+                                        if e.value:
+                                            print(f'Joining {device.player_name} to {coordinator.player_name}')
+                                            device.join(coordinator)
+                                        else:
+                                            print(f'Unjoining {device.player_name} from {coordinator.player_name}')
+                                            device.unjoin()
+                                        system.update_devices()
+                                        speakers_ui.refresh()
+                                    ui.checkbox(device.player_name, value=device in devices) \
+                                        .on_value_change(update_group) \
+                                        .set_enabled(device != coordinator)
+
                 ui.separator()
+
                 with ui.grid(columns='repeat(2, auto)').classes('items-center gap-y-0 w-full px-4'):
-                    for device in devices:
+                    for device in sorted(devices, key=lambda d: (d is not coordinator, d.player_name)):
                         ui.label(device.player_name).classes('text-lg font-bold')
                         with ui.row(align_items='center'):
                             ui.checkbox(value=not device.mute) \
